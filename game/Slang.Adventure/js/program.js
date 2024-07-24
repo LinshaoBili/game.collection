2627626036f35c6e09db5eb7e1aa2cf71075851d
LinshaoBili <52987757+LinshaoBili@users.noreply.github.com>
2024-07-24 23:42:03 +0800

    Add files via upload

--- game/Slang.Adventure/js/program.js
+++ game/Slang.Adventure/js/program.js
@@ -35,8 +35,40 @@ function Initial_loading() {
   //游戏界面设置
   subject.style.userSelect = "none";
 
-  //预加载图像
-  var id = [];
+  //键盘检测
+  window.onkeydown = function (event) {
+    Operate(event.key.toLowerCase());
+    console.log("keyboard " + event.key.toLowerCase());
+  };
+
+  //新增快捷栏
+  var menu = document.getElementById("ls-m-home");
+  let new_menu = document.createElement("span");
+  new_menu.innerHTML = "查看版本";
+  new_menu.setAttribute("class", "background");
+  new_menu.onclick = function () {
+    m_d_c_active();
+    notification(
+      "游戏版本",
+      "<div style='text-align: center'>" + VERSION + "</div>",
+      "bottom",
+      0,
+      "0.5s",
+      "wait"
+    );
+  };
+  menu.appendChild(new_menu);
+  //快捷栏改正
+  menu = menu.getElementsByClassName("background");
+  for (let index = 0; index < menu.length; index++) {
+    menu[index].style.margin = "0px 15px";
+  }
+}
+
+//预加载图像
+function Preload() {
+  display_Loading = [];
+  id = [];
   var scr = [];
   var caches = [];
   var params = getDefaultJSON("Player", "textures");
@@ -78,43 +110,21 @@ function Initial_loading() {
     }
   }
   for (let index = 0; index < id.length; index++) {
-    var textures = new Image();
+    let textures = new Image();
     textures.src = "textures/" + scr[index];
     textures.onload = function () {
-      console.log("Loading complete > " + scr[index]);
+      var text = "Loading complete > " + scr[index];
+      display_Loading[display_Loading.length] = text;
+      console.log(text);
+      Loading("display");
+      Home("Home");
     };
     textures.onerror = function () {
-      error(id[index] + " Loading > " + scr[index]);
+      var text = id[index] + " Loading > " + scr[index];
+      display_Loading[display_Loading.length] = "Error > " + text;
+      error(text);
+      Loading("display");
     };
-  }
-
-  //键盘检测
-  window.onkeydown = function (event) {
-    Operate(event.key.toLowerCase());
-    console.log("keyboard " + event.key.toLowerCase());
-  };
-
-  //新增快捷栏
-  var menu = document.getElementById("ls-m-home");
-  let new_menu = document.createElement("span");
-  new_menu.innerHTML = "查看版本";
-  new_menu.setAttribute("class", "background");
-  new_menu.onclick = function () {
-    m_d_c_active();
-    notification(
-      "游戏版本",
-      "<div style='text-align: center'>" + VERSION + "</div>",
-      "bottom",
-      0,
-      "0.5s",
-      "wait"
-    );
-  };
-  menu.appendChild(new_menu);
-  //快捷栏改正
-  menu = menu.getElementsByClassName("background");
-  for (let index = 0; index < menu.length; index++) {
-    menu[index].style.margin = "0px 15px";
   }
 }
 
@@ -155,15 +165,25 @@ function Operate(keydown) {
 //主界面
 function Home(params) {
   if (params == "Home") {
-    Loading("background", "#131516");
-    notification(
-      "提醒",
-      "若有开启插件请关闭以免影响游戏体验<br>破解复制类 'Dark Reader' 等插件",
-      "top",
-      500,
-      "1s",
-      "wait"
-    );
+    if (!document.getElementById("Loading")) {
+      Loading('<div class="display-Loading"></div>', "#131516");
+      Preload();
+      notification(
+        "提醒",
+        "若有开启插件请关闭以免影响游戏体验<br>破解复制类 'Dark Reader' 等插件",
+        "top",
+        500,
+        "1s",
+        "wait"
+      );
+    } else {
+      if (id.length == display_Loading.length) {
+        Map("MainInterface");
+        setTimeout(function () {
+          Loading("false");
+        }, 1000);
+      }
+    }
   } else {
     Loading(subject.innerHTML, "#131516");
   }
@@ -172,57 +192,75 @@ function Home(params) {
 //加载画面
 function Loading(text, color) {
   if (text != "false") {
-    var LoadingElement = document.createElement("div");
-    LoadingElement.setAttribute("id", "Loading");
-    if (text == "background") {
-      LoadingElement.style.backgroundColor = color;
-    } else {
-      LoadingElement.innerHTML = text;
-    }
-    LoadingElement.style.position = "absolute";
-    LoadingElement.style.width = "100%";
-    LoadingElement.style.height = "100%";
-    LoadingElement.style.top = "0px";
-    LoadingElement.style.left = "0px";
-    var LoadingAnimation = document.createElement("div");
-    LoadingAnimation.setAttribute("class", "LoadingAnimation");
-    LoadingAnimation.style.position = "absolute";
-    LoadingAnimation.style.width = "7.5%";
-    LoadingAnimation.style.display = "flex";
-    LoadingAnimation.style.bottom = "2.5%";
-    LoadingAnimation.style.right = "2.5%";
-    LoadingAnimation.style.justifyContent = "center";
-    LoadingAnimation.style.alignItems = "center";
-    LoadingAnimation.style.transform = "translate(-50%,-50%)";
-    var LoadingText = document.createElement("b");
-    LoadingText.setAttribute("class", "LoadingText");
-    LoadingText.style.margin = "0px auto";
-    LoadingText.innerHTML = "Loading";
-    var LoadingIcon = document.createElement("div");
-    LoadingIcon.setAttribute("class", "LoadingIcon");
-    LoadingIcon.style.position = "absolute";
-    LoadingIcon.style.borderWidth = LoadingIconSize();
-    LoadingAnimation.appendChild(LoadingText);
-    LoadingAnimation.appendChild(LoadingIcon);
-    if (document.getElementById("Loading")) {
-      document.getElementById("Loading").appendChild(LoadingAnimation);
-    } else {
-      LoadingElement.appendChild(LoadingAnimation);
-      subject.appendChild(LoadingElement);
-    }
-    setTimeout(function () {
-      var LoadingAnimation =
-        document.getElementsByClassName("LoadingAnimation");
-      for (let index = 0; index < LoadingAnimation.length; index++) {
-        LoadingAnimation[index].style.opacity = "1";
+    if (text == "display") {
+      var element = document.getElementsByClassName("display-Loading");
+      for (let index = 0; index < element.length; index++) {
+        element[index].innerHTML = display_Loading[display_Loading.length - 1];
       }
-    }, 500);
+    } else {
+      var LoadingElement = document.createElement("div");
+      LoadingElement.setAttribute("id", "Loading");
+      if (color) {
+        LoadingElement.style.backgroundColor = color;
+      }
+      LoadingElement.innerHTML = text;
+      LoadingElement.style.position = "absolute";
+      LoadingElement.style.width = "100%";
+      LoadingElement.style.height = "100%";
+      LoadingElement.style.top = "0px";
+      LoadingElement.style.left = "0px";
+      LoadingElement.style.zIndex = "5";
+      var LoadingAnimation = document.createElement("div");
+      LoadingAnimation.setAttribute("class", "LoadingAnimation");
+      LoadingAnimation.style.position = "absolute";
+      LoadingAnimation.style.width = "7.5%";
+      LoadingAnimation.style.display = "flex";
+      LoadingAnimation.style.bottom = "2.5%";
+      LoadingAnimation.style.right = "2.5%";
+      LoadingAnimation.style.justifyContent = "center";
+      LoadingAnimation.style.alignItems = "center";
+      LoadingAnimation.style.transform = "translate(-50%,-50%)";
+      var LoadingText = document.createElement("b");
+      LoadingText.setAttribute("class", "LoadingText");
+      LoadingText.style.margin = "0px auto";
+      LoadingText.innerHTML = "Loading";
+      var LoadingIcon = document.createElement("div");
+      LoadingIcon.setAttribute("class", "LoadingIcon");
+      LoadingIcon.style.position = "absolute";
+      LoadingIcon.style.borderWidth = LoadingIconSize();
+      LoadingAnimation.appendChild(LoadingText);
+      LoadingAnimation.appendChild(LoadingIcon);
+      if (document.getElementById("Loading")) {
+        document.getElementById("Loading").appendChild(LoadingAnimation);
+      } else {
+        LoadingElement.appendChild(LoadingAnimation);
+        subject.appendChild(LoadingElement);
+      }
+      setTimeout(function () {
+        var LoadingAnimation =
+          document.getElementsByClassName("LoadingAnimation");
+        for (let index = 0; index < LoadingAnimation.length; index++) {
+          LoadingAnimation[index].style.opacity = "1";
+        }
+      }, 500);
+    }
   } else {
     var Loading = document.getElementById("Loading");
-    Loading.style.opacity = "0";
+    var LoadingAnimation = document.getElementsByClassName("LoadingAnimation");
+    var displayLoading = document.getElementsByClassName("display-Loading");
+    for (let index = 0; index < LoadingAnimation.length; index++) {
+      LoadingAnimation[index].style.opacity = "0";
+    }
+    for (let index = 0; index < displayLoading.length; index++) {
+      displayLoading[index].style.transition = "0.5s";
+      displayLoading[index].style.opacity = "0";
+    }
     setTimeout(function () {
-      Loading.remove();
-    }, 2000);
+      Loading.style.opacity = "0";
+      setTimeout(function () {
+        Loading.remove();
+      }, 2000);
+    }, 1500);
   }
 }
 
@@ -245,21 +283,72 @@ function Map(id) {
       element.style.margin = "-1px";
       element.style.width = 100 + "%";
       if (Map) {
-        if (Map["textures"]) {
-          if (Map["textures"] != null) {
-            element.style.backgroundImage =
-              "url(textures/block/" + Map["textures"] + ")";
+        var list = ["textures", "rotate"];
+        function random(params) {
+          //random
+          if (params == "rotate") {
+            var params = Math.floor(Math.random() * 4);
+            if (params == 0) {
+              params = "0deg";
+            } else {
+              if (params == 1) {
+                params = "90deg";
+              } else {
+                if (params == 2) {
+                  params = "180deg";
+                } else {
+                  params = "270deg";
+                }
+              }
+            }
           }
-        } else {
-          var json = getDefaultJSON("block", Map["block"] + "|textures");
-          if (json) {
-            if (json != "null") {
-              element.style.backgroundImage =
-                "url(textures/block/" + json + ")";
+          return params;
+        }
+        for (let index = 0; index < list.length; index++) {
+          if (Map[list[index]]) {
+            //
+            if (Map[list[index]] != null) {
+              //
+              if (index == 0) {
+                element.style.backgroundImage =
+                  "url(textures/block/" + Map[list[index]] + ")";
+              } else {
+                if (index == 1) {
+                  if (Map[list[index]] == "random") {
+                    element.style.transform =
+                      "rotate(" + random("rotate") + ")";
+                  }
+                }
+              }
             }
           } else {
-            element.style.backgroundImage =
-              "url(textures/block/" + Map["block"] + ".png)";
+            //
+            var json = getDefaultJSON(
+              "block",
+              Map["block"] + "|" + list[index]
+            );
+            if (json) {
+              //
+              if (json != "null") {
+                //
+                if (index == 0) {
+                  element.style.backgroundImage =
+                    "url(textures/block/" + json + ")";
+                }
+                if (index == 1) {
+                  if (json == "random") {
+                    element.style.transform =
+                      "rotate(" + random("rotate") + ")";
+                  }
+                }
+              }
+            } else {
+              //
+              if (index == 0) {
+                element.style.backgroundImage =
+                  "url(textures/block/" + Map["block"] + ".png)";
+              }
+            }
           }
         }
         element.style.backgroundRepeat = "no-repeat";
